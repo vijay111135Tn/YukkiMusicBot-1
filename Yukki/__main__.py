626b71fec6605425cd3d561b673a9fec7db0d71e
vijay111135Tn <88264345+vijay111135Tn@users.noreply.github.com>
2021-12-10 18:59:18 +0530

    Update __main__.py

--- Yukki/__main__.py
+++ Yukki/__main__.py
@@ -121,7 +121,7 @@ async def initiate_bot():
 
 home_text_pm = f"""Hᴇʟʟᴏ,
 Mʏ Nᴀᴍᴇ Is {BOT_NAME}.
-A Tᴇʟᴇɢʀᴀᴍ Mᴜsɪᴄ Bᴏᴛ Aʟʟᴏᴡs Yᴏᴜ Tᴏ Pʟᴀʏ Mᴜsɪᴄ Oɴ Gʀᴏᴜᴘs Tʜʀᴏᴜɢʜ Tʜᴇ Nᴇᴡ Tᴇʟᴇɢʀᴀᴍ's Vᴏɪᴄᴇ Cʜᴀᴛs Pᴏᴡᴇʀ Bʏ ⚡PʏTɢM𝐮𝐬𝐢𝐜 !.
+A Tᴇʟᴇɢʀᴀᴍ Mᴜsɪᴄ Bᴏᴛ Aʟʟᴏᴡs Yᴏᴜ Tᴏ Pʟᴀʏ Mᴜsɪᴄ Oɴ Gʀᴏᴜᴘs Tʜʀᴏᴜɢʜ Tʜᴇ Nᴇᴡ Tᴇʟᴇɢʀᴀᴍ's Vᴏɪᴄᴇ Cʜᴀᴛs Pᴏᴡᴇʀ Bʏ ⚡[PʏTɢM𝐮𝐬𝐢𝐜](https://telegra.ph/file/a2332a552646efe87114d.jpg) !.
 
 Mᴀᴅᴇ Wɪᴛʜ ❤️ Bʏ [𝐕𝐢𝐣𝐚𝐲](t.me/Attitude_king_vj)"""
 
